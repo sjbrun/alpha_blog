7e87239b5b3191498e45f717c8b794c579f4b164
Sam Bruno <sjbrunster@gmail.com>
2016-03-22 22:57:24 +0000

    Extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  ## run "set_article" method before specified methods
+  before_action :set_article, only: [:edit,:update,:show,:destroy]
+  
   def index
     @articles = Article.all
   end
@@ -8,7 +11,7 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+    
   end
   
   def create
@@ -22,7 +25,6 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated."
       redirect_to article_path(@article)
@@ -32,18 +34,20 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted."
     redirect_to articles_path
   end
   
   private
-  def article_params  # whitelist method
-    params.require(:article).permit(:title, :description)
-  end
+    def set_article  ## create private method to reuse
+      @article = Article.find(params[:id])
+    end
+    def article_params  # whitelist method
+      params.require(:article).permit(:title, :description)
+    end
 end

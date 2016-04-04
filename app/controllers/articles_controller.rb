5f920aaf51eed73ca93821abc8a6b2a38c739ba3
Sam Bruno <sjbrunster@gmail.com>
2016-04-04 21:24:38 +0000

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   ## run "set_article" method before specified methods
   before_action :set_article, only: [:edit,:update,:show,:destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -48,7 +50,15 @@ class ArticlesController < ApplicationController
     def set_article  ## create private method to reuse
       @article = Article.find(params[:id])
     end
+    
     def article_params  # whitelist method
       params.require(:article).permit(:title, :description)
+    end
+    
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to root_path
+      end
     end
 end

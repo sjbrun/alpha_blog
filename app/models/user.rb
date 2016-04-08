224a8be69936df14c9de0150d8e51b8da058a436
Sam Bruno <sjbrunster@gmail.com>
2016-04-08 21:34:54 +0000

    Add admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase }  ## ensures email is saved to db as all lowercase
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false }, 
